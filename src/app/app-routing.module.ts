a57269634030170096d41ade5a35361c2f6a55cb
Jarg <ja_reyes1124@hotmail.com>
2020-06-08 15:34:18 -0500

    Version 01-Sidemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '/schedule',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'calendar',
+    loadChildren: () => import('./Menu/calendar/calendar.module').then( m => m.CalendarPageModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./Menu/news/news.module').then( m => m.NewsPageModule)
+  },
+  {
+    path: 'schedule',
+    loadChildren: () => import('./Menu/schedule/schedule.module').then( m => m.SchedulePageModule)
+  },
 ];
 
 @NgModule({
